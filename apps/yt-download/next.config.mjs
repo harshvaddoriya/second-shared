26ce3812611b41ccb8372fb88fc2ac58f2091517
Harsh Vaddoriya <harshvaddoriya0319@gmail.com>
2025-09-26 11:50:22 +0530

    change link

--- apps/yt-download/next.config.mjs
+++ apps/yt-download/next.config.mjs
@@ -20,7 +20,7 @@ const nextConfig = {
 
         filename: 'static/chunks/remoteEntry.js',
         remotes: {
-          shared: `shared@http://localhost:3002/_next/static/chunks/remoteEntry.js`,
+          //shared: `shared@http://localhost:3002/_next/static/chunks/remoteEntry.js`,
           shared: `shared@https://second-shared-shared.vercel.app/_next/static/chunks/remoteEntry.js`,
         },
         shared: {
