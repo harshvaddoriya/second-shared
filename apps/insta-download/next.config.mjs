69797af8a08adf33222095519f01e2a4dd5b6036
Harsh Vaddoriya <harshvaddoriya0319@gmail.com>
2025-10-07 10:48:53 +0530

    add style changes

--- apps/insta-download/next.config.mjs
+++ apps/insta-download/next.config.mjs
@@ -28,8 +28,8 @@ const nextConfig = {
         name: 'instaDownload',
         filename: 'static/chunks/remoteEntry.js',
         remotes: {
-          // shared: `shared@http://localhost:3002/_next/static/chunks/remoteEntry.js`,
-          shared: `shared@https://second-shared-shared.vercel.app/_next/static/chunks/remoteEntry.js`,
+          shared: `shared@http://localhost:3002/_next/static/chunks/remoteEntry.js`,
+          //shared: `shared@https://second-shared-shared.vercel.app/_next/static/chunks/remoteEntry.js`,
         },
         shared: {
           react: { singleton: true, requiredVersion: false },
